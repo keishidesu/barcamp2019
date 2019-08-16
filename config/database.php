d2fe3a65dac6d95561bf09eb251d77e91275d42f
keishidesu <caseytea0840@gmail.com>
2019-08-16 23:26:00 +0800

    update database

--- config/database.php
+++ config/database.php
@@ -45,17 +45,15 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'db4free.net'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'itsbarcamp'),
+            'username' => env('DB_USERNAME', 'barcamp'),
+            'password' => env('DB_PASSWORD', 'barcamp2019'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
             'prefix' => '',
-            'prefix_indexes' => true,
             'strict' => true,
             'engine' => null,
             'options' => extension_loaded('pdo_mysql') ? array_filter([
@@ -63,6 +61,7 @@ return [
             ]) : [],
         ],
 
+        
         'pgsql' => [
             'driver' => 'pgsql',
             'url' => env('DATABASE_URL'),
