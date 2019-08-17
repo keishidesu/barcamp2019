1d9019a447c191d0f19e540e76bb64a2d271c5af
keishidesu <caseytea0840@gmail.com>
2019-08-17 11:51:37 +0800

    test1

--- public/js/vote.js
+++ public/js/vote.js
@@ -21,13 +21,13 @@ $('.vote').on('click', function(event){
 	})
 	.done(function(){
 		event.target.innerText = isVote ? event.target.innerText == 'Vote' ? 'You voted for this talk' : 'Vote' :
-		event.target.innerText == 'Downvote' ? 'You downvoted this talk' : 'Downvote'; 
+		// event.target.innerText == 'Downvote' ? 'You downvoted this talk' : 'Downvote'; 
 
-		if(isVote){
-			event.target.nextElementSibling.innerText = 'Downvote';
-		} else{
-			event.target.previousElementSibling.innerText = 'Vote';
-		}
+		// if(isVote){
+		// 	event.target.nextElementSibling.innerText = 'Downvote';
+		// } else{
+		// 	event.target.previousElementSibling.innerText = 'Vote';
+		// }
 
 	})
 	.fail(function(err){
